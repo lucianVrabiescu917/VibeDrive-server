9b135e5ab12cd48c73f94bb3bf935339e3b16084
917_Lucian_Vrabiescu <72062803+lucianVrabiescu917@users.noreply.github.com>
2024-01-10 20:59:46 +0200

    server setup commit

--- src/main/java/com/example/vibedrive/VibeDriveApplication.java
+++ src/main/java/com/example/vibedrive/VibeDriveApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class VibeDriveApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(VibeDriveApplication.class, args);
     }
