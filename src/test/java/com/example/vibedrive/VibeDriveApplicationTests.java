d3f8637a9cd7cc79dfe493b715ac62afd251323e
917_Lucian_Vrabiescu <72062803+lucianVrabiescu917@users.noreply.github.com>
2024-01-10 20:42:46 +0200

    server setup commit

---
+++ src/test/java/com/example/vibedrive/VibeDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vibedrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeDriveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
